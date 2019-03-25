dca4763bfcaeecf358cd70cf3924801520424046
Ralph Deus <RD043481@digicelgroup.com>
2019-03-25 15:32:13 -0400

    First Commit

---
+++ aedig/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+import smtplib
+
+
+def index(request):
+	return render(request, 'index.html', {})
+
+
+def message(request):
+	if request.method == 'POST':
+		msg = request.POST.get('msg')
+		formData= request.POST.get('formData')
+		data = json.loads(formData)
+		name, subject, email, message = '', '', '', ''
+
+		name = data[1]['value']
+		email = data[2]['value']
+		subject = data[3]['value']
+		message = data[4]['value']
+
+		infos = 'Nom: ' + name + '\nemail: ' + email + '\nSujet: ' + subject + '\nMessage: ' + message
+		template = 'Salut Mr RalphWinner, Nouveau Message!!!!! \n ' + infos
+
+		#Code for the email Sender... Lolllllz
+
+		server = smtplib.SMTP('smtp.gmail.com', 25)
+		server.connect("smtp.gmail.com",587)
+		server.ehlo()
+		server.starttls()
+		server.ehlo()
+		server.login('ralphrezososyal@gmail.com', "mariage090197")
+		server.sendmail('ralphrezososyal@gmail.com', 'rallralph.haver@gmail.com', template)
+		server.quit()
+
+	return HttpResponse('Votre Message a ete envoyé avec Succes !!!')
---
+++ static/contactform/Readme.txt
@@ -0,0 +1,6 @@
+Fully working Asp.Net c# backend Just Contact Ralphlaa The Winner Babyyy! at Ralph.Deus@code9haiti.com
+
+
+
+
+
